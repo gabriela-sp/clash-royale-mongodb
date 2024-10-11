05792b136eeca12f1b519156319fef645f3dd57a
Aira Soares <114684137+Airassilva@users.noreply.github.com>
2024-10-11 01:55:21 +0000

    Add ingests

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,12 @@ import { IntegracaoController } from './integracao/integracao.controller';
 import { IntegracaoService } from './integracao/integracao.service';
 import { IntegracaoModule } from './integracao/integracao.module';
 import { HttpModule } from '@nestjs/axios';
+import { NewAppService } from './integracao/new-app.service';
 
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true }), MongoConnection, MongooseModule.forFeature([{ name: Player.name, schema: BattleSchema }]), IntegracaoModule],
   controllers: [AppController, IntegracaoController],
-  providers: [AppService, IntegracaoService],
+  providers: [AppService, IntegracaoService, NewAppService],
 })
 export class AppModule { }
