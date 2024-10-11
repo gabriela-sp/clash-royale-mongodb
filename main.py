6904e9e65f948accc274313f7dd7326bd9b972b5
Aira Soares <114684137+Airassilva@users.noreply.github.com>
2024-10-11 00:29:10 +0000

    ajustes

--- main.py
+++
@@ -1,6 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://gabrielapereira:admin@cluster0.wbnna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-client = MongoClient(uri, server_api=ServerApi('1'))
